95cf5be5626984bda93e7c918476c0402e5adeda
Olivier MEDOC <o_medoc@yahoo.fr>
2021-11-18 11:21:54 +0100

    resolve Parameter ref

--- pyswagger/scanner/v3_0_0/resolve.py
+++ pyswagger/scanner/v3_0_0/resolve.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 from ...errs import CycleDetectionError
 from ...scan import Dispatcher
-from ...spec.v3_0_0.parser import SchemaContext
-from ...spec.v3_0_0.objects import Schema
+from ...spec.v3_0_0.parser import SchemaContext,ParameterContext
+from ...spec.v3_0_0.objects import Schema,Parameter
 
 class Resolve(object):
     """ pre-resolve '$ref' """
@@ -19,6 +19,23 @@ class Resolve(object):
             return
 
         ro = app.resolve(r, SchemaContext)
+        if not ro:
+            raise ReferenceError('Unable to resolve: {0}'.format(r))
+        if ro.__class__ != obj.__class__:
+            raise TypeError('Referenced Type mismatch: {0}'.format(r))
+
+        obj.update_field('ref_obj', ro)
+
+    @Disp.register([Parameter])
+    def _parameter(self, _, obj, app):
+        if obj.ref_obj:
+            return
+
+        r = getattr(obj, '$ref')
+        if not r:
+            return
+
+        ro = app.resolve(r, ParameterContext)
         if not ro:
             raise ReferenceError('Unable to resolve: {0}'.format(r))
         if ro.__class__ != obj.__class__:
